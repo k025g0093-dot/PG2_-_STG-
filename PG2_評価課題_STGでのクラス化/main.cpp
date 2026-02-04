2f669cf7da15b9afc5bfbd2afc6548041e82a9e6
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-02-04 15:16:14 +0900

    タイトルの変更

--- PG2_評価課題_STGでのクラス化/main.cpp
+++ PG2_評価課題_STGでのクラス化/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <Novice.h>
 #include "Game.h"
-const char kWindowTitle[] = "LC1B_31_ヤマトユウヤ_タイトル";
+const char kWindowTitle[] = "LC1B_31_ヤマトユウヤ_幾何学世界での戦い";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
